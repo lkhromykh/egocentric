async wrapper is fixed with the help of cloudpickle (it should be listed in requirements) gradient steps fusion done physics error occurring: it should be fixed. As of now only problems with actions range are considered but tfd.TruncatedNormal is not working for whatever reason also need to be carefully with the critic initialization

src/rl/alg.py
@@ -87,7 +87,6 @@ def vpi(cfg: Config, nets: Networks) -> StepFn:
         }
         return actor_loss + critic_loss, metrics
 
-    @chex.assert_max_traces(1)
     def step(state: TrainingState,
              batch: types.Trajectory
              ) -> tuple[TrainingState, types.Metrics]:
@@ -105,4 +104,19 @@ def vpi(cfg: Config, nets: Networks) -> StepFn:
         state = state.update(grads)
         return state.replace(rng=rng), metrics
 
-    return step
+    @chex.assert_max_traces(1)
+    def fuse(state: TrainingState,
+             batch: types.Trajectory,
+             ) -> tuple[TrainingState, types.Metrics]:
+        batch_dim = 1
+        num_steps = jax.tree_util.tree_leaves(batch)[0].shape[batch_dim]
+        num_steps //= cfg.batch_size
+        for i in range(num_steps):
+            b = cfg.batch_size * i
+            e = b + cfg.batch_size
+            subbatch = tree_slice(batch, jnp.s_[:, b:e])
+            state, metrics = step(state, subbatch)
+        print('Tracing done')
+        return state, metrics
+
+    return fuse

src/rl/builder.py
@@ -5,13 +5,14 @@ import numpy as np
 import jax
 import optax
 import haiku as hk
-from rltools.dmc_wrappers import AutoReset, AsyncEnv, SequentialEnv
+from rltools.dmc_wrappers import AutoReset, AsyncEnv
 
 from src.rl.replay_buffer import ReplayBuffer
 from src.rl.networks import Networks
 from src.rl.config import Config
 from src.rl.training_state import TrainingState
 from src.rl.alg import vpi, StepFn
+from src.rl.ops.environment import FromOneHot
 from src.rl import types_ as types
 
 
@@ -30,12 +31,14 @@ class Builder:
     def make_env(self, rng: int) -> dm_env.Environment:
         c = self.cfg
         rng = np.random.default_rng(rng)
+
+        def wrap(env): return AutoReset(FromOneHot(env))
         match c.task.split('_'):
             case 'dmc', domain, task:
                 from dm_control import suite
 
                 def env_fn(seed):
-                    return lambda: AutoReset(suite.load(
+                    return lambda: wrap(suite.load(
                         domain, task,
                         task_kwargs={'random': seed},
                         environment_kwargs={'flat_observation': True}
@@ -44,16 +47,17 @@ class Builder:
                 from src.suite import load
 
                 def env_fn(seed):
-                    return lambda: AutoReset(load(
+                    return lambda: wrap(load(
                         seed,
                         action_mode=c.action_space,
                         img_size=(84, 84),
-                        time_limit=5,
+                        control_timestep=.05,
+                        time_limit=2.5,
                     ))
             case _:
                 raise ValueError(self.cfg.task)
         seeds = rng.integers(0, np.iinfo(np.int32).max, c.num_envs)
-        return SequentialEnv([env_fn(seed) for seed in seeds])
+        return AsyncEnv([env_fn(seed) for seed in seeds], context='spawn')
 
     def make_networks(self, env: dm_env.Environment) -> Networks:
         return Networks.make_networks(

src/rl/networks.py
@@ -27,12 +27,12 @@ class MLP(hk.Module):
         self.layers = layers
         self.act = act
         self.norm = norm
-        self.activation_final = activate_final
+        self.activate_final = activate_final
 
     def __call__(self, x: Array) -> Array:
         for idx, layer in enumerate(self.layers):
             x = hk.Linear(layer)(x)
-            if idx != len(self.layers) - 1 or self.activation_final:
+            if idx != len(self.layers) - 1 or self.activate_final:
                 x = _get_norm(self.norm)(x)
                 x = _get_act(self.act)(x)
         return x
@@ -109,17 +109,23 @@ class Actor(hk.Module):
 
     def __call__(self, state: Array) -> tfd.Distribution:
         state = MLP(self.layers, self.act, self.norm)(state)
-
+        w_init = hk.initializers.TruncatedNormal(.01)
         match sp := self.action_spec:
             case specs.DiscreteArray():
-                logits = hk.Linear(sp.num_values)(state)
+                logits = hk.Linear(sp.num_values, w_init=w_init)(state)
                 dist = tfd.OneHotCategorical(logits)
             case specs.BoundedArray():
-                fc = hk.Linear(2 * sp.shape[0])
+                fc = hk.Linear(2 * sp.shape[0], w_init=w_init)
                 mean, std = jnp.split(fc(state), 2, -1)
                 mean = jnp.tanh(mean)
                 std = jax.nn.sigmoid(std) + 1e-3
                 dist = tfd.Normal(mean, std)
+                # TODO: Find out why truncated normal causing nans
+                # dist = tfd.TruncatedNormal(
+                #     mean, std,
+                #     low=sp.minimum.astype(mean.dtype),
+                #     high=sp.maximum.astype(mean.dtype),
+                # )
                 dist = tfd.Independent(dist, 1)
             case _:
                 raise ValueError(sp)
@@ -145,7 +151,7 @@ class Critic(hk.Module):
                  ) -> Array:
         x = jnp.concatenate([state, action.astype(state.dtype)], -1)
         x = MLP(self.layers, self.act, self.norm)(x)
-        fc = hk.Linear(1)
+        fc = hk.Linear(1, w_init=jnp.zeros)
         return fc(x)
 
 

src/rl/ops/environment.py
@@ -4,6 +4,7 @@ import collections
 import dm_env.specs
 import numpy as np
 from jax.tree_util import tree_map
+from rltools.dmc_wrappers.base import Wrapper
 from rltools.dmc_wrappers import AutoReset
 
 from src.rl import types_ as types
@@ -14,10 +15,24 @@ ActionLogProbFn = Callable[
 ]
 
 
-def from_one_hot(env, action):
-    if isinstance(env.action_spec(), dm_env.specs.DiscreteArray):
-        action = action.argmax(-1)
-    return action
+class FromOneHot(Wrapper):
+    """Convert one-hot action to its value + cache action space."""
+
+    def __init__(self, env: dm_env.Environment) -> None:
+        super().__init__(env)
+        act_spec = env.action_spec()
+        self._is_discrete = isinstance(act_spec, dm_env.specs.DiscreteArray)
+        if not self._is_discrete:
+            act_spec = act_spec.replace(dtype=np.float32)
+        self._act_spec = act_spec
+
+    def step(self, action: types.Action) -> dm_env.TimeStep:
+        if self._is_discrete:
+            action = action.argmax(-1)
+        return super().step(action)
+
+    def action_spec(self) -> dm_env.specs.Array:
+        return self._act_spec
 
 
 def train_loop(env: AutoReset,  # continue interacting after termination
@@ -26,12 +41,11 @@ def train_loop(env: AutoReset,  # continue interacting after termination
                prev_timestep: dm_env.TimeStep | None = None,
                ) -> tuple[types.Trajectory, dm_env.TimeStep]:
     trajectory = collections.defaultdict(list)
-    def act_transform(act): return from_one_hot(env, act)
     ts = prev_timestep or env.reset()
     for _ in range(num_steps):
         obs = ts.observation
         action, log_prob = policy(obs)
-        ts = env.step(act_transform(action))
+        ts = env.step(np.asarray(action))
         trajectory['observations'].append(obs)
         trajectory['actions'].append(action)
         trajectory['rewards'].append(ts.reward)
@@ -52,10 +66,9 @@ def eval_loop(env: dm_env.Environment, policy: ActionLogProbFn) -> np.float:
     shape = np.asanyarray(ts.step_type).shape
     cont = np.ones(shape, dtype=bool)
     reward = np.zeros(shape, dtype=env.reward_spec().dtype)
-    def act_transform(act): return from_one_hot(env, act)
     while cont.any():
         action, _ = policy(ts.observation)
-        ts = env.step(act_transform(action))
+        ts = env.step(np.asarray(action))
         reward += cont * ts.reward
         cont *= np.logical_not(ts.last())
     return reward

src/rl/ops/retrace.py
@@ -10,13 +10,14 @@ def retrace(q_t: chex.Array,
             log_rho_t: chex.Array,
             lambda_: float
             ) -> chex.Array:
+    """1606.02647"""
     rho_t = lambda_ * jnp.minimum(1., jnp.exp(log_rho_t))
     xs = (q_t, v_tp1, r_t, disc_t, rho_t)
     chex.assert_rank(xs, 1)
     chex.assert_equal_shape(xs)
     chex.assert_scalar_non_negative(lambda_)
 
-    def fn(acc, x):  # 1606.02647
+    def fn(acc, x):
         q, next_v, r, disc, c = x
         resid = r + disc * next_v - q
         acc = resid + disc * c * acc

src/suite/tasks/base.py
@@ -55,7 +55,7 @@ class WorkSpace(NamedTuple):
     @classmethod
     def from_halfsizes(cls,
                        half_sizes: tuple[float, float] = (.1, .1),
-                       tcp_height: tuple[float, float] = (.162, .4)
+                       tcp_height: tuple[float, float] = (.1, .3)
                        ) -> 'WorkSpace':
         x, y = half_sizes
         low, high = tcp_height
@@ -79,7 +79,7 @@ class Task(abc.ABC, _Task):
         self._control_timestep = control_timestep
         self.action_mod = action_mode.lower()
         self.workspace = workspace
-        h, w = self.img_size = img_size
+        self.img_size = h, w = img_size
 
         self._arena = entities.Arena()
         self._gripper = entities.Robotiq2f85()
@@ -119,17 +119,20 @@ class Task(abc.ABC, _Task):
     def before_step(self, physics, action, random_state):
         if self.action_mod == 'discrete':
             action = DiscreteActions.as_array(action)
+        else:
+            action = np.clip(action, -1, 1)
         pos, grip = np.split(action, [3])
         mocap_pos, _ = self._get_mocap(physics)
         self._set_mocap(physics, mocap_pos + common.CTRL_LIMIT * pos)
         self._gripper.set_grasp(physics, float(grip > 0.))
 
     def action_spec(self, physics):
+        num_values = len(DiscreteActions)
         match self.action_mod:
             case 'discrete':
-                return specs.DiscreteArray(len(DiscreteActions))
+                return specs.DiscreteArray(num_values)
             case 'continuous':
-                lim = np.full((4,), 1, dtype=np.float32)
+                lim = np.full((num_values // 2,), 1, dtype=np.float32)
                 return specs.BoundedArray(
                     shape=lim.shape,
                     dtype=lim.dtype,