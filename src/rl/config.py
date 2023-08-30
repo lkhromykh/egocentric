434ee02ea2164a33482644440e5c6e5ba9251085
lkhromykh <leonidgm@gmail.com>
2023-08-30 14:29:02 +0300

    global average pooling version + layer_norm + larger conv

--- src/rl/config.py
+++ src/rl/config.py
@@ -16,21 +16,21 @@ class Config(_Config):
     activation: str = 'elu'
     normalization: str = 'layer'
     asymmetric: bool = True
-    mlp_layers: Layers = (256,)
-    cnn_depths: Layers = (64, 64, 64, 64)
+    mlp_layers: Layers = (512,)
+    cnn_depths: Layers = (96, 96, 96, 96)
     cnn_kernels: Layers = (3, 3, 3, 3)
     cnn_strides: Layers = (2, 2, 2, 2)
     actor_keys: str = r'image|tcp_pose|object_detected|gripper_pos'
-    actor_layers: Layers = (256, 256)
+    actor_layers: Layers = (512, 512)
     critic_keys: str = r'robotiq_2f85|model'
-    critic_layers: Layers = (256, 256)
+    critic_layers: Layers = (512, 512)
     ensemble_size: int = 2
 
     # Train
     jit: bool = True
-    buffer_capacity: int = 10 ** 5
-    batch_size: int = 256
-    sequence_len: int = 4
+    buffer_capacity: int = 5 * 10 ** 5
+    batch_size: int = 512
+    sequence_len: int = 2
     utd: float = .2
     learning_rate: float = 3e-4
     init_temperature: float = 1e-2
@@ -41,7 +41,7 @@ class Config(_Config):
     eval_every: int = 20_000
     train_after: int = 10_000
 
-    logdir: str = 'logdir/dr_image_textured_boxesncylinders'
+    logdir: str = 'logdir/dr_image_textured_boxesncylinders_GAP'
     task: str = 'src'
     action_space: str = 'discrete'
     num_envs: int = 16
